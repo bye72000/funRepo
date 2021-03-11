fb34cddf4045aeb30580c04df0488a66387e6253
Bing Ye <bye72000@gmail.com>
2021-03-10 23:24:52 -0500

    add py files

---
+++ gralib/calc_data.py
@@ -0,0 +1,21 @@
+
+
+def add(a, b):
+    return a+b
+
+def mult(a, b):
+    return a*b
+
+def addMult(a,b):
+    x = add(a,b)
+    y = mult(a,b)
+    return x*y
+#
+# def main():
+#     z = addMult(5,4)
+#     print(z)
+#
+# if __name__ == '__main__':
+#     main()
+
+
---
+++ gralib/calc_data_1.py
@@ -0,0 +1,6 @@
+from gralib.calc_data import add, mult
+
+def addMult(a,b):
+    x = add(a,b)
+    y = mult(a,b)
+    return x*y
